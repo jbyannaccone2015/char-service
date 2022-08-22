043ff4dd0e02db7adb79723f24f41f271ff18b72
jby <justin@yannaccone.net>
2022-08-22 11:39:42 -0400

    deployment state

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
 const cors = require('cors')
-const PORT = process.env.PORT || "8080"
 require('dotenv').config()
 app.use(cors({ origin: ["http://localhost:3000"], credentials: true}))
+app.use(cors({ origin: ["https://gatekeep.netlify.app/"], credentials: true}))
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.vmuwpnk.mongodb.net/gkinfo`)
 
@@ -14,5 +14,5 @@ app.use('/character', require('./routes/characterRouter'))
 app.use('/characters', require('./routes/userBasedRouter'))
 
 app.listen(process.env.PORT, () => {
-    console.log(`The server is running on ${PORT}`)
+    console.log(`The server is running on ${process.env.PORT}`)
 })
