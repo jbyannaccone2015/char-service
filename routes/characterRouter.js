f7be6f188e0377ef2675de57e87bef5c07f19adf
jby <justin@yannaccone.net>
2022-08-23 13:53:04 -0400

    corsMiddleware added

--- routes/characterRouter.js
+++ routes/characterRouter.js
@@ -1,8 +1,10 @@
 const { getOneCharacter, deleteCharacter, updateCharacter, createCharacter } = require('../controllers/CharacterController')
 const authMiddleware = require('../middleware/auth')
+const corsMiddleware = require('../middleware/cors')
 
 const router = require('express').Router()
 router.use(authMiddleware)
+router.use(corsMiddleware)
 
 router.get('/:id', getOneCharacter)
 router.delete('/:id', deleteCharacter)
--- routes/userBasedRouter.js
+++ routes/userBasedRouter.js
@@ -1,8 +1,10 @@
 const { getAllCharacters } = require('../controllers/CharacterController')
 const authMiddleware = require('../middleware/auth')
+const corsMiddleware = require('../middleware/cors')
 
 const router = require('express').Router()
 router.use(authMiddleware)
+router.use(corsMiddleware)
 
 router.get('/', getAllCharacters)
 
